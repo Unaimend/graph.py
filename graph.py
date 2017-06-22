4181035bcfc1cea4a560f02214be54b22cd0d372
tdOSX <Unaimend@gmail.com>
2017-06-22 18:25:21 +0200

    Tja, ne Mene passiert :)

--- graph.py
+++ graph.py
@@ -1,63 +1,97 @@
 import tkinter as tk
 
-import math
-
+class Vector():
+    def __init__(self, x, y):
+        self.x = x;
+        self.y = y;
 
 class Window:
     def __init__(self, root):
-        self.root = root;
+        self.root = root
+
+        # Init. canvas
         self.canvas = tk.Canvas(self.root, relief=tk.SUNKEN, bd=4,
                                 width=1200, height=800,  background='white')
+        # Sth. with the layout
         self.canvas.pack()
 
+        # Mouse and keyboard bindings
         self.root.bind("<Button 1>", self.createNodeAtMousePos)
-
         self.root.bind("<g>", self.changeNodeLook)
-
         self.root.bind("<c>", self.clearCanvas)
 
-
+        # Specifies the minimal distance two nodes are allowed to have
         self.graphNodesMinDistance = 2*GraphNode.graphNodeRadius;
 
+        # Array for the the nodes of the graph
         self.graphNodes = []
+        # Array for the the edges of the graph
         self.graphEdges = []
-
+        # Array which represents the isConnectedTo relationship
+        self.adjacencyList = []
+        # Saves the coordinates of the last two clicked notes
         self.clickedNodes = []
-
+        # Specifies whether the node ids should be drawn or not
         self.drawNodeIds = False;
-
+        # Helper variable for the node ids
         self.nodeCounter = 0;
 
-    def clearCanvas(self, event):
+
+        self.menubar = tk.Menu(self.root)
+        # File menu
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit")
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+
+        # View menu
+        self.viewmenu = tk.Menu(self.menubar, tearoff=0)
+        self.viewmenu.add_command(label="Toggle ids     (g)", command=self.changeNodeLook)
+        self.viewmenu.add_command(label="Clear canvas   (cg)", command=self.clearCanvas)
+        self.menubar.add_cascade(label="View", menu=self.viewmenu)
+
+        self.root.config(menu=self.menubar)
+
+
+    def clearCanvas(self, event="nothing"):
+        # clear canvas
         self.canvas.delete("all")
+        # clear nodes and edges
         self.graphNodes = []
         self.graphEdges = []
+        # reset nodeCounter and also the ids
         self.nodeCounter = 0
 
 
-    def changeNodeLook(self, event):
+    def changeNodeLook(self, event="nothing"):
         self.canvas.delete("all")
 
-        alternativNodeList = []
-        alternativEdgeList = []
+
         if not self.drawNodeIds:
             self.drawNodeIds = True
         else:
             self.drawNodeIds = False;
+
+        self.redrawNodes()
+        self.redrawEdges()
+
+    def redrawNodes(self):
+        alternativNodeList = []
         for node in self.graphNodes:
             alternativNodeList.append(GraphNode(self.canvas, node.x,
-                                            node.y, "", self.drawNodeIds, node.id))
-
-        for edge in self.graphEdges:
-            alternativEdgeList.append(GraphEdge(self.canvas, edge.x0,
-                                                edge.y0, edge.endX, edge.endY))
-
-
+                                                node.y, "", self.drawNodeIds, node.id))
         self.graphNodes = alternativNodeList
+
+    def redrawEdges(self):
+        alternativEdgeList = []
+        for edge in self.graphEdges:
+            alternativEdgeList.append(GraphEdge(self.canvas, edge.start.x,
+                                                edge.start.y, edge.end.x, edge.end.y))
         self.graphEdges = alternativEdgeList
 
     def run(self):
         self.root.mainloop()
+
     def createNodeAtMousePos(self,event):
         isInCircle = False
         for node in self.graphNodes:
@@ -113,38 +147,18 @@ class GraphNode():
 
 class GraphEdge():
     def __init__(self, canvas, x0, y0, xn, yn):
-        self.x0 = x0
-        self.y0 = y0
+        # Start der Kante
+        self.start = Vector(x0, y0)
+        # Ender der Katen
+        self.end = Vector(xn, yn)
 
-        self.endX = xn;
-        self.endY = yn;
 
-        self.middleX = 0
-        self.middleY = 0
 
-        print("x0", self.x0, "y0", self.y0)
 
-        print("endX", self.endX, "endY", self.endY)
-        # # startpunkt links endpunkt
-        # if x0 <=xn:
-        #     # startpunkt unter von endpunkt
-        #     if y0 >= yn:
-        #         self.middleX = self.x0 + (self.endX - self.x0)/2
-        #         self.middleY = self.y0 + (self.endY - self.y0)/2
-        #
-        #         canvas.create_line(x0, y0, self.middleX, self.middleY, smooth=True)
-        #
-        #
-        #         print("MiddleX", self.middleX, "MiddleY", self.middleY)
-        #     # startpunkt ueber von endpunkt
-        #     else:
-        #         print("mimimimimi")
-        # # startpunkt rechts vom entpunkt
-        # else:
-        #     print("mimimimimi")
-        #     # startpunkt unter von endpunkt
-        #     # startpunkt ueber von endpunkt
-        canvas.create_line(x0, y0, xn, yn, smooth=True)
+        # Debugausgaben
+        # print("x0", self.x0, "y0", self.y0)
+        # print("endX", self.endX, "endY", self.endY)
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, smooth=True)
 
 
 
