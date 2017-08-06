570dd715c43872de5b2fafd1dd750516b75e2cac
tdOSX <Unaimend@gmail.com>
2017-08-06 17:51:32 +0200

    ...

--- test_eades.py
+++ test_eades.py
@@ -1,4 +1,4 @@
-import Eades
+from Eades import Eades
 import unittest
 from graph import GraphNode
 import tkinter as tk
@@ -10,21 +10,21 @@ class EadesTest(unittest.TestCase):
     def test_distance(self):
         node1 = GraphNode(canvas=tk.Canvas(), x=0,y=0, id=1, drawIds=False, text="")
         node2 = GraphNode(canvas=tk.Canvas(), x=0,y=200, id=2, drawIds=False, text="")
-        self.assertEqual(Eades.Eades.distance(node1=node1, node2=node2), 200)
+        self.assertEqual(Eades.distance(node1=node1, node2=node2), 200)
 
         node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, drawIds=False, text="")
         node2 = GraphNode(canvas=tk.Canvas(), x=200, y=0, id=2, drawIds=False, text="")
-        self.assertEqual(Eades.Eades.distance(node1=node1, node2=node2), 200)
+        self.assertEqual(Eades.distance(node1=node1, node2=node2), 200)
 
         node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, drawIds=False, text="")
         node2 = GraphNode(canvas=tk.Canvas(), x=100, y=100, id=2, drawIds=False, text="")
-        self.assertTrue(math.isclose(Eades.Eades.distance(node1=node1, node2=node2), 141.421, abs_tol=0.001))
+        self.assertTrue(math.isclose(Eades.distance(node1=node1, node2=node2), 141.4, abs_tol=0.1))
         # TODO 3 oder 4 weitere unit test schreiben damit alles funktioniert
 
     def test_unit_vector(self):
         node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, drawIds=False, text="")
         node2 = GraphNode(canvas=tk.Canvas(), x=0, y=200, id=2, drawIds=False, text="")
-        unit = Eades.Eades.unit_vector(node1, node2)
+        unit = Eades.unit_vector(node1, node2)
         # distance = 0-0 = 0, node1.x - node2.x = 0-0 = 0
         self.assertEqual(unit.x, 0)
         self.assertEqual(unit.y, -1)
