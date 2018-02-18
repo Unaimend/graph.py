f2a285d126796d87a3704c220640ee6aa9d67745
tdOSX <Unaimend@gmail.com>
2018-02-18 21:34:17 +0100

    commented out a few print statements

--- eades.py
+++ eades.py
@@ -32,16 +32,14 @@ class Eades:
 
     # 2,200,5000,0.1
 
-
     @staticmethod
-    # TODO UNITTESSSSTTSSSSSSS
-    def distance(node1: graph.GraphNode = None, node2: graph.GraphNode = None):
+    def distance(node1: graph.GraphNode, node2: graph.GraphNode) -> float:
         # Pythagorean theorem in R^2(euclidean distance in R^2)
         distance = math.sqrt((node1.position.x - node2.position.x) ** 2 + (node1.position.y - node2.position.y) ** 2)
         return distance
 
     @staticmethod
-    def unit_vector(node1: graph.GraphNode = None, node2: graph.GraphNode = None):
+    def unit_vector(node1: graph.GraphNode, node2: graph.GraphNode) -> float:
         # Calculate x and y distance separate
         v = Vector(node1.position.x - node2.position.x, node1.position.y - node2.position.y)
         # Divide vector by its length to obtain a unit vector
@@ -64,7 +62,7 @@ class Eades:
                     displacement.y += direction.y  * attractive_force * Eades.c4
             node.move(displacement.x, displacement.y )
 
-    # Sollte die "repelling force" ueber alle vertices berechnet werden oder nur die nicht-adjazenten
+    @staticmethod
     def calculate_repelling_force_for_all_nodes_and_move_accordingly_new():
         for node in Eades.graph_visuals.graphNodes:
             displacement = Vector(0, 0)
@@ -79,33 +77,6 @@ class Eades:
                     displacement.x += direction.x * repelling_force * Eades.c4
                     displacement.y += direction.y * repelling_force * Eades.c4
             node.move(displacement.x, displacement.y )
-
-
-    @staticmethod
-    def calculate_attractive_force_for_all_nodes_and_move_accordingly_old():
-        # TODO Exception falls Eades.graph == None
-        for node in Eades.graph_visuals.graphNodes:
-            for nodes in Eades.graph_visuals.node_adjacency_list[node.id]:
-                # If if would calc. the distance between two node which have the same id, the distance would be 0
-                # and that would mean that I would divide by 0 in the attractive_force calculation
-                if node.id != nodes.id:
-                    distance = Eades.distance(node, nodes)
-                    attractive_force = Eades.c1 * math.log(distance / Eades.c2)
-                    direction = Eades.unit_vector(nodes, node)
-                    node.move_old(direction.x * attractive_force * Eades.c4, direction.y * attractive_force * Eades.c4)
-
-    @staticmethod
-    def calculate_repelling_force_for_all_nodes_and_move_accordingly_old(event=None):
-        for node in Eades.graph_visuals.graphNodes:
-            for nodes in Eades.graph_visuals.graphNodes:
-                if node.id != nodes.id:
-                    distance = Eades.distance(node, nodes)
-                    repelling_force = (Eades.c3 / (distance ** 2))
-                    direction = Eades.unit_vector(node, nodes)
-                    node.move_old(direction.x * repelling_force * Eades.c4, direction.y * repelling_force  * Eades.c4)
-
-
-
 
 
 
--- fr.py
+++ fr.py
@@ -18,16 +18,16 @@ class FruchtermanReingold:
     # In dem Paper hat jede Node ein displacement attribut welchens bei meiner Implementierung nicht vorhanden ist
     # dieses Attribut simuliere ich dadurch das ich fuer jede node die verschiebung berechne und in der Liste
     # speichere. an [0] steht also der Wert um den die Node mit der Id 0 verschoben werden soll etc.
-    @staticmethod
     # TODO UNITTESSSSTTSSSSSSS
-    def distance(node1: graph.GraphNode = None, node2: graph.GraphNode = None):
+    @staticmethod
+    def distance(node1: graph.GraphNode, node2: graph.GraphNode) -> float:
         # Pythagorean theorem in R^2(euclidean distance in R^2)
         distance = math.sqrt((node1.position.x - node2.position.x) ** 2 + (node1.position.y - node2.position.y) ** 2)
         return distance
 
 
     @staticmethod
-    def unit_vector(node1: graph.GraphNode = None, node2: graph.GraphNode = None):
+    def unit_vector(node1: graph.GraphNode, node2: graph.GraphNode) -> float:
         # Calculate x and y distance separate
         v = Vector(node1.position.x - node2.position.x, node1.position.y - node2.position.y)
         # Divide vector by its length to obtain a unit vector
@@ -35,22 +35,21 @@ class FruchtermanReingold:
         return v
 
     @staticmethod
-    def fr(z):
+    def fr(z: float) -> float:
         if FruchtermanReingold.k is None:
-           raise ValueError("Area and k must be precalculated")
+            raise ValueError("Area and k must be precalculated")
         else:
-            return ((FruchtermanReingold.k ** 2) / z)
-    def fa(z):
+            return (FruchtermanReingold.k ** 2) / z
+    @staticmethod
+    def fa(z : float) -> float:
         if FruchtermanReingold.k is None:
-           raise ValueError("Area and k must be precalculated")
+            raise ValueError("Area and k must be precalculated")
         else:
-            return ((z ** 2) / FruchtermanReingold.k)
-
-
+            return (z ** 2) / FruchtermanReingold.k
 
     @staticmethod
     def calc_repelling_forces():
-        print("LENGTH", len(FruchtermanReingold.displacement_list))
+        #print("LENGTH", len(FruchtermanReingold.displacement_list))
         #TODO WIe sorgge ich dafuer das die dispalcement lsite hier garantiert lange genug ist
         # Diese MEthode berechnet die abstossenden Kraefte
         #BUG  Ist diese Liste hier moeglicherweise mit nicht nullwerete gefuellt was den algo. kaputmacht
@@ -59,28 +58,26 @@ class FruchtermanReingold:
                 if node.id != nodes.id:
                     diff = node.position - nodes.position
                     diff_length = diff.abs()
-                    print("FR", FruchtermanReingold.fr(diff_length))
+                    #print("FR", FruchtermanReingold.fr(diff_length))
                     scaled_x = diff.to_unit().x * FruchtermanReingold.fr(diff_length)
                     scaled_y = diff.to_unit().y * FruchtermanReingold.fr(diff_length)
                     scaled_unit = Vector(scaled_x, scaled_y)
                     FruchtermanReingold.displacement_list[node.id] = FruchtermanReingold.displacement_list[node.id] + scaled_unit
 
-
-
     @staticmethod
     def calc_attractive_forces():
         for edge in FruchtermanReingold.graph_visuals.graphEdges:
 
-            print("START", edge.start_node.position, "END", edge.end_node.position)
+            #print("START", edge.start_node.position, "END", edge.end_node.position)
             diff = edge.start_node.position - edge.end_node.position
 
-            print("Difference", diff)
+            #print("Difference", diff)
             diff_length = diff.abs()
-            print("FA", FruchtermanReingold.fa(diff_length))
+            #print("FA", FruchtermanReingold.fa(diff_length))
             scaled_x = diff.to_unit().x * FruchtermanReingold.fa(diff_length)
             scaled_y = diff.to_unit().y * FruchtermanReingold.fa(diff_length)
             scaled_unit = Vector(scaled_x, scaled_y)
-            print("SCU", scaled_unit)
+            #print("SCU", scaled_unit)
             FruchtermanReingold.displacement_list[edge.start_node.id] = FruchtermanReingold.displacement_list[edge.start_node.id] - scaled_unit
 
             FruchtermanReingold.displacement_list[edge.end_node.id] = FruchtermanReingold.displacement_list[edge.end_node.id] + scaled_unit
