718e7ae274ddf29b82a38f12425e2a0c237dd9bb
tdOSX <Unaimend@gmail.com>
2018-09-06 11:38:19 +0200

    Made class non-static

--- fr.py
+++ fr.py
@@ -4,7 +4,6 @@ import math
 import graph
 from vector import Vector
 
-
 # Dises Modul implementiert den Layouting-Algorithmus von T. Fruchterman und E. Reingold
 # aus dem Paper Graph Drawing by Force-directed Placement [FR91]
 
@@ -12,14 +11,16 @@ from vector import Vector
 class FruchtermanReingold:
     """ Class which implements the layouting-algorithm from EAD[84]
     """
-    graph_visuals = None
-    canvas_width = None
-    canvas_length = None
-    displacement_list = []
+    def __init__(self, graph_visuals, canvas_width, canvas_height, k, t):
+        self.graph_visuals = graph_visuals
+        self.canvas_width = canvas_width
+        self.canvas_length = canvas_height
+        self.displacement_list = []
 
-    area = None
-    k = None
-    t = 0
+        self.area = canvas_width * canvas_height
+        self.k = k
+        self.t = t
+
     # In dem Paper hat jede Node ein displacement attribut welchens bei meiner Implementierung
     # nicht vorhanden ist dieses Attribut simuliere ich dadurch das ich fuer jede node die verschiebung
     # berechne und in der Liste speichere. an [0] steht also der Wert um den die Node mit der Id 0
@@ -57,33 +58,47 @@ class FruchtermanReingold:
         unit_vec = Vector(x_comp, y_comp)
         return unit_vec
 
-    @staticmethod
-    def fr(distance: float) -> float:
+
+    def do_fr(self):
+        for x in range(0, 100):
+            # Das hier in Funk. do_fr_one_iter
+            self.displacement_list = [Vector(0, 0)] * self.graph_visuals.nodeCounter
+            self.calc_attractive_forces()
+
+            self.calc_repelling_forces()
+            i = 0
+            for disp in self.displacement_list:
+                v = min(disp.abs(), self.t)
+                direction = Vector(disp.to_unit().x * v, disp.to_unit().y * v )
+                self.graph_visuals.graphNodes[i].move(direction.x, direction.y)
+                i = i + 1
+            self.cool()
+
+
+    def fr(self, distance: float) -> float:
         """
         Calculates the repulsive force for a given distance
         :param distance:  
         :return: The repulsive force for the distance
         """
-        if FruchtermanReingold.k is None:
+        if self.k is None:
             # TODO Hier formel hinschreiben die im paper verwendet werden
             raise ValueError("Area and k must be precalculated(see paper)")
         else:
-            return (FruchtermanReingold.k ** 2) / distance
+            return (self.k ** 2) / distance
 
-    @staticmethod
-    def fa(distance: float) -> float:
+    def fa(self, distance: float) -> float:
         """
         Calculates the attractive force for the distance
         :param distance:  
         :return: The attractive force for the distance
         """
-        if FruchtermanReingold.k is None:
+        if self.k is None:
             raise ValueError("Area and k must be precalculated(see paper)")
         else:
-            return (distance ** 2) / FruchtermanReingold.k
+            return (distance ** 2) / self.k
 
-    @staticmethod
-    def calc_repelling_forces():
+    def calc_repelling_forces(self):
         """
         Calculates and "anwenden" the attractive forces 
         """
@@ -91,41 +106,39 @@ class FruchtermanReingold:
         # Diese MEthode berechnet die abstossenden Kraefte
         # BUG  Ist diese(displacement list) Liste hier moeglicherweise
         # mit nicht nullwerete gefuellt was den algo. kaputmacht
-        for node in FruchtermanReingold.graph_visuals.graphNodes:
-            for nodes in FruchtermanReingold.graph_visuals.graphNodes:
+        for node in self.graph_visuals.graphNodes:
+            for nodes in self.graph_visuals.graphNodes:
                 if node.id != nodes.id:
                     diff = node.position - nodes.position
                     diff_length = diff.abs()
-                    scaled_x = diff.to_unit().x * FruchtermanReingold.fr(diff_length)
-                    scaled_y = diff.to_unit().y * FruchtermanReingold.fr(diff_length)
+                    scaled_x = diff.to_unit().x * self.fr(diff_length)
+                    scaled_y = diff.to_unit().y * self.fr(diff_length)
                     scaled_unit = Vector(scaled_x, scaled_y)
-                    new_force = FruchtermanReingold.displacement_list[node.id] + scaled_unit
-                    FruchtermanReingold.displacement_list[node.id] = new_force
+                    new_force = self.displacement_list[node.id] + scaled_unit
+                    self.displacement_list[node.id] = new_force
 
-    @staticmethod
-    def calc_attractive_forces():
+    def calc_attractive_forces(self):
         """
         Calculates and "anwenden" the attractive forces 
         """
-        for edge in FruchtermanReingold.graph_visuals.graphEdges:
+        for edge in self.graph_visuals.graphEdges:
             diff = edge.start_node.position - edge.end_node.position
             diff_length = diff.abs()
-            scaled_x_comp = diff.to_unit().x * FruchtermanReingold.fa(diff_length)
-            scaled_y_comp = diff.to_unit().y * FruchtermanReingold.fa(diff_length)
+            scaled_x_comp = diff.to_unit().x * self.fa(diff_length)
+            scaled_y_comp = diff.to_unit().y * self.fa(diff_length)
             scaled_unit = Vector(scaled_x_comp, scaled_y_comp)
             # Nomma ins paper schauen
-            start_force = FruchtermanReingold.displacement_list[edge.start_node.id] - scaled_unit
-            end_force = FruchtermanReingold.displacement_list[edge.end_node.id] + scaled_unit
-            FruchtermanReingold.displacement_list[edge.start_node.id] = start_force
-            FruchtermanReingold.displacement_list[edge.end_node.id] = end_force
+            start_force = self.displacement_list[edge.start_node.id] - scaled_unit
+            end_force = self.displacement_list[edge.end_node.id] + scaled_unit
+            self.displacement_list[edge.start_node.id] = start_force
+            self.displacement_list[edge.end_node.id] = end_force
 
-    @staticmethod
-    def cool():
+    def cool(self):
         """
         Implements the cooling from the paper
         :return: Nothing 
         """
-        FruchtermanReingold.t = FruchtermanReingold.t - 1
+        self.t = self.t - 1
 
 
 
