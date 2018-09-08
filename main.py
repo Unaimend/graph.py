0e8f619d75845f2d621ae7c8ac71e28c35554a60
tdOSX <Unaimend@gmail.com>
2018-09-08 23:16:10 +0200

    Added project path to python mdoule search path

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import tkinter as tk
+import sys
 import window as win
+
+sys.path.append("~/Documents/dev/Projects/graph.py")
 
 if __name__ == "__main__":
     # Addded to test TravisC
     # exit(0)
     window = win.Window(tk.Tk())
     window.run()
+
