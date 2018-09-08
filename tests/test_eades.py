db29ab50862417b38aa384074cafb2ea6c217b3e
tdOSX <Unaimend@gmail.com>
2018-09-09 00:31:55 +0200

    ..

--- tests/test_eades.py
+++ tests/test_eades.py
@@ -11,21 +11,21 @@ from graph import GraphNode
 class EadesTest(unittest.TestCase):
 
     def test_distance(self):
-        node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, draw_ids=False)
-        node2 = GraphNode(canvas=tk.Canvas(), x=0, y=200, id=2, draw_ids=False)
+        node1 = GraphNode(canvas=None, x=0, y=0, id=1, draw_ids=False)
+        node2 = GraphNode(canvas=None, x=0, y=200, id=2, draw_ids=False)
         self.assertEqual(distance(node1=node1, node2=node2), 200)
 
-        node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, draw_ids=False)
-        node2 = GraphNode(canvas=tk.Canvas(), x=200, y=0, id=2, draw_ids=False)
+        node1 = GraphNode(canvas=None, x=0, y=0, id=1, draw_ids=False)
+        node2 = GraphNode(canvas=None, x=200, y=0, id=2, draw_ids=False)
         self.assertEqual(distance(node1=node1, node2=node2), 200)
 
-        node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, draw_ids=False)
-        node2 = GraphNode(canvas=tk.Canvas(), x=100, y=100, id=2, draw_ids=False)
+        node1 = GraphNode(canvas=None, x=0, y=0, id=1, draw_ids=False)
+        node2 = GraphNode(canvas=None, x=100, y=100, id=2, draw_ids=False)
         self.assertTrue(math.isclose(distance(node1=node1, node2=node2), 141.4, abs_tol=0.1))
 
     def test_unit_vector(self):
-        node1 = GraphNode(canvas=tk.Canvas(), x=0, y=0, id=1, draw_ids=False)
-        node2 = GraphNode(canvas=tk.Canvas(), x=0, y=200, id=2, draw_ids=False)
+        node1 = GraphNode(canvas=None, x=0, y=0, id=1, draw_ids=False)
+        node2 = GraphNode(canvas=None, x=0, y=200, id=2, draw_ids=False)
         unit = unit_vector(node1, node2)
         # distance = 0-0 = 0, node1.x - node2.x = 0-0 = 0
         self.assertEqual(unit.x, 0)
